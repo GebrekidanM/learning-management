16c9079a039aefac78d1482dd8c1577243ef4ab1
GebrekidanM <designerkidan@gmail.com>
2024-08-04 01:13:53 +0300

    navbar

--- front/src/component/header/Navbar.js
+++ front/src/component/header/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import logo from "../../asset/logo.png"
+import style from "../css/Navbar.module.css"
 function Navbar() {
+  const [logged, setLogged] = useState()
+
   return (
-    <div>
-      <ul>
-        <li><a><img src="../../asset/logo.png" alt='nitsuh'/></a></li>
+    <div className={style.navContainer}>
+      <ul className={style.navBox}>
+        <span><a><img src={logo} alt='nitsuh' className={style.logo}/></a></span>
         <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Blog</li>
       </ul>
+      <div className={style.logBox}>
+        {logged ? <span>kidan</span>:<span className={style.button}>Login</span>}
+      </div>
     </div>
   )
 }
