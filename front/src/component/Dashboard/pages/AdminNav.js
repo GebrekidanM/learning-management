bde3b3341b971e393aac9951e1293eadc55aa52d
GebrekidanM <designerkidan@gmail.com>
2024-08-08 23:06:45 +0300

    Dashboard headpart

--- front/src/component/Dashboard/pages/AdminNav.js
+++ front/src/component/Dashboard/pages/AdminNav.js
@@ -4,7 +4,7 @@ import style from '../../css/Dashbord.module.css'
 function AdminNav({username}) {
   return (
     <div className={style.adminNav}>
-      <div>
+      <div className={style.username}>
           Welcome {username}
       </div>
 
